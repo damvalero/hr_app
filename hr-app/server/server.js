27776eb7c4adb4d45fcbde0bf66b9e980819ab55
damvalero <dmendesvalero@gmail.com>
2022-02-14 19:54:35 +0100

    added cors

--- hr-app/server/server.js
+++ hr-app/server/server.js
@@ -1,9 +1,11 @@
 // require('dotenv').config();
 
 const express = require('express');
-const path = require('path');
 const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
+
+app.use(cors());
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nawqx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
 const db= mongoose.connection;
@@ -14,16 +16,8 @@ app.use(express.urlencoded({extended: true}));
 // this line substitude bodyParser, express already does that
 app.use(express.json());
 
-//to make work the heroku deploy
-// app.use(express.static(path.join('public')))
-
 // Routes
 const employeesRouter = require('./routes/employees-routes.js');
-
-// passing build folder of the client to the public in server
-// app.use((req, res, next) => {
-//     res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
-// })
 
 // app will go to the routes
 app.use('/employees', employeesRouter);
